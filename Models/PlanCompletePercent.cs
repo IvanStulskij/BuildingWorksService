2d272a99af85749a6209441574b0db05e53ec3c7
Ivan Stulskij <ivanstulskij@gmail.com>
2023-01-27 18:27:58 +0300

    created planCompletePercentEntity and added MathConstants

---
+++ Models/PlanCompletePercent.cs
@@ -0,0 +1,19 @@
+﻿namespace Models
+{
+    public class PlanCompletePercent
+    {
+        private readonly float _totalAmount;
+        private readonly float _completeAmount;
+
+        public PlanCompletePercent(float totalAmount, float completeAmount)
+        {
+            _totalAmount = totalAmount;
+            _completeAmount = completeAmount;
+        }
+
+        public float Count()
+        {
+            return _completeAmount / _totalAmount;
+        }
+    }
+}
