4694f4d46ddfad892a3e72fad79fa626143993e4
Ivan Stulskij <ivanstulskij@gmail.com>
2023-01-28 22:31:08 +0300

    developed materialsPriceService

---
+++ Models/Services/Implementations/Providers/MaterialsPriceService.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BuildingWorks.Models.Databasable.Tables.Provides;
+using Models;
+using Models.Repositories.Abstractions.Providers;
+using Models.Resources.Providers;
+
+namespace BuildingWorks.Models.Services.Implementations.Providers
+{
+    public class MaterialsPriceService : Service<ContractsByMaterials, MaterialResource, MaterialForm>
+    {
+        public MaterialsPriceService(BuildingWorksDbContext context, Mapper mapper) : base(context, mapper)
+        {
+        }
+
+        public override IMaterialPriceRepository Repository { get; }
+    }
+}
