895097e86c5b5a22ad8403ffeb1ebe5490b91d3f
Ivan Stulskij <ivanstulskij@gmail.com>
2023-01-28 22:13:09 +0300

    fixed providerService

---
+++ Models/Services/Interfaces/Providers/IProviderService.cs
@@ -0,0 +1,17 @@
+﻿using Models.Resources.Providers;
+
+namespace BuildingWorks.Models.Services.Interfaces.Providers
+{
+    public interface IProviderService
+    {
+        Task<IEnumerable<ProviderResource>> GetAll();
+
+        Task<ProviderResource> GetById(int id);
+
+        Task<ProviderResource> Delete(int id);
+
+        Task<ProviderResource> Create(ProviderForm provider);
+
+        Task<ProviderResource> Update(int id, ProviderForm provider);
+    }
+}
