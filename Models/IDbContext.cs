e22e18f485e81e08eaa711434840fba67490c4bd
Ivan Stulskij <ivanstulskij@gmail.com>
2023-01-28 10:06:45 +0300

    renamed models to buildingWorks.models

---
+++ Models/IDbContext.cs
@@ -0,0 +1,30 @@
+﻿using BuildingWorks.Models.Databasable.Tables.BuildingObjects;
+using BuildingWorks.Models.Databasable.Tables.BuildingObjects.Address;
+using BuildingWorks.Models.Databasable.Tables.Plans;
+using BuildingWorks.Models.Databasable.Tables.Provides;
+using Microsoft.EntityFrameworkCore;
+using BuildingWorks.Models.Databasable.Tables.Registration;
+using BuildingWorks.Models.Databasable.Tables.Workers;
+
+namespace Models.Contexts
+{
+    public interface IDbContext
+    {
+        public DbSet<Provider> Providers { get; set; }
+        public DbSet<Contract> Contracts { get; set; }
+        public DbSet<Material> Materials { get; set; }
+        public DbSet<ContractsByMaterials> ContractsByMaterials { get; set; }
+        public DbSet<BuildingObject> BuildingObject { get; set; }
+        public DbSet<ObjectAddress> ObjectAddress { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Town> Towns { get; set; }
+        public DbSet<Street> Streets { get; set; }
+        public DbSet<Plan> Plans { get; set; }
+        public DbSet<PlanDetail> PlansDetails { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UnregisteredUserCode> UnregisteredUsersCodes { get; set; }
+        public DbSet<Worker> Workers { get; set; }
+        public DbSet<Brigade> Brigades { get; set; }
+        public DbSet<WorkerSalary> WorkersSalaries { get; set; }
+    }
+}
