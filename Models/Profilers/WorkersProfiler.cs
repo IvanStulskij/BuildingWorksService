17ac5eae715f2b422cd745b62e1ab4c6072a4c4d
Ivan Stulskij <ivanstulskij@gmail.com>
2023-01-29 15:24:29 +0300

    developed workersProfiler

---
+++ Models/Profilers/WorkersProfiler.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BuildingWorks.Models.Databasable.Tables.Workers;
+using Models.Resources.Workers;
+
+namespace BuildingWorks.Models.Profilers
+{
+    public class WorkersProfiler : Profile
+    {
+        public WorkersProfiler()
+        {
+            CreateMap<Worker, WorkerResource>().ReverseMap();
+        }
+    }
+}
