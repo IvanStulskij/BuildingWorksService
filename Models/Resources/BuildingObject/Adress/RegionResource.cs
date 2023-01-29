a27e46ce1a692ccb7477204f39ad6c397d2fec6c
Ivan Stulskij <ivanstulskij@gmail.com>
2023-01-29 16:44:35 +0300

    fixed building object resources

--- Models/Resources/BuildingObject/Adress/RegionResource.cs
+++ Models/Resources/BuildingObject/Adress/RegionResource.cs
@@ -5,8 +5,6 @@ namespace Models.Resources.BuildingObject.Adress
     public class RegionResource : RegionForm, IResource 
     {
         public int Id { get; set; }
-
-        public int RegionCode { get; set; }
     }
 
     public class RegionForm
