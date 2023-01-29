e020b0bf49c5928a3ea53e07460cbd1a31231368
Ivan Stulskij <ivanstulskij@gmail.com>
2023-01-29 16:45:16 +0300

    fixed plan resources (removed old id)

--- Models/Resources/Plans/PlanDetailResource.cs
+++ Models/Resources/Plans/PlanDetailResource.cs
@@ -5,8 +5,6 @@ namespace Models.Resources.Plans
     public class PlanDetailResource : IResource
     {
         public int Id { get; set; }
-
-        public int PlanDetailCode { get; set; }
     }
 
     public class PlanDetailForm
