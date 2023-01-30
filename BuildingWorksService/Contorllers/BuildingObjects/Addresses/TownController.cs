8f80d1278257c43ee0d941cb7fc2797c581ec644
Ivan Stulskij <ivanstulskij@gmail.com>
2023-01-30 16:37:17 +0300

    developed town controller

---
+++ BuildingWorksService/Contorllers/BuildingObjects/Addresses/TownController.cs
@@ -0,0 +1,87 @@
+﻿using BuildingWorks.Models.Services.Interfaces.BuildingObjects;
+using Microsoft.AspNetCore.Mvc;
+using Models.Resources.BuildingObject.Addresses;
+
+namespace BuildingWorksService.Contorllers.BuildingObjects.Addresses
+{
+    [ApiController]
+    [Route("/api/towns")]
+    public class TownController : ControllerBase
+    {
+        private readonly ITownService _service;
+
+        public TownController(ITownService service)
+        {
+            _service = service;
+        }
+
+        /// <summary>
+        /// Get town by id.
+        /// </summary>
+        /// <param name="id"> Id to get town. </param>
+        /// <returns> Single town. </returns>
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(TownResource), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetById([FromQuery] int id)
+        {
+            var town = await _service.GetById(id);
+
+            return Ok(town);
+        }
+
+        /// <summary>
+        /// Get all towns.
+        /// </summary>
+        /// <returns> The list of towns. </returns>
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<TownResource>), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetAll()
+        {
+            var towns = await _service.GetAll();
+
+            return Ok(towns);
+        }
+
+        /// <summary>
+        /// Create town.
+        /// </summary>
+        /// <param name="form"> Form to create town. </param>
+        /// <returns> Created town. </returns>
+        [HttpPost]
+        [ProducesResponseType(typeof(TownResource), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Create([FromBody] TownForm form)
+        {
+            var response = await _service.Create(form);
+
+            return Ok(response);
+        }
+
+        /// <summary>
+        /// Delete town by id.
+        /// </summary>
+        /// <param name="id"> Id to delete town. </param>
+        /// <returns> Deleted town. </returns>
+        [HttpDelete("{id}")]
+        [ProducesResponseType(typeof(TownResource), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Delete([FromQuery] int id)
+        {
+            var response = await _service.Delete(id);
+
+            return Ok(response);
+        }
+
+        /// <summary>
+        /// Update town.
+        /// </summary>
+        /// <param name="resource"> Resource to update town. </param>
+        /// <returns> Updated town. </returns>
+        [HttpPut]
+        [ProducesResponseType(typeof(TownResource), StatusCodes.Status200OK)]
+        public async Task<IActionResult> Update([FromBody] TownResource resource)
+        {
+            var response = await _service.Update(resource.Id, resource);
+
+            return Ok(response);
+        }
+    }
+}
