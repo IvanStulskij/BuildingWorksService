593a4afdec9938b0d73426af9295aca016edbf78
Ivan Stulskij <ivanstulskij@gmail.com>
2023-01-30 17:05:21 +0300

    added service collection registration

--- BuildingWorksService/Extensions/ServiceCollectionExtensions.cs
+++ BuildingWorksService/Extensions/ServiceCollectionExtensions.cs
@@ -1,7 +1,15 @@
 ﻿using BuildingWorks.Models.Services.Implementations.Plans;
+using BuildingWorks.Models.Services.Implementations.Providers;
+using BuildingWorks.Models.Services.Implementations.Workers;
 using BuildingWorks.Models.Services.Interfaces.Plans;
+using BuildingWorks.Models.Services.Interfaces.Providers;
+using BuildingWorks.Models.Services.Interfaces.Workers;
 using Models.Repositories.Abstractions.Plans;
+using Models.Repositories.Abstractions.Providers;
+using Models.Repositories.Abstractions.Workers;
 using Models.Repositories.Implementations.Plans;
+using Models.Repositories.Implementations.Providers;
+using Models.Repositories.Implementations.Workers;
 using Models.Services;
 using Models.Services.Implementations;
 
@@ -13,16 +21,24 @@ namespace BuildingWorksService.Extensions
         {
             services.AddScoped<IPlanService, PlanService>();
             services.AddScoped<IPlanDetailsService, PlanDetailsService>();
-            /*services.AddScoped<IProviderService, ProviderService>();
-            services.AddScoped<IPlanService, PlanService>();*/
+            services.AddScoped<IProviderService, ProviderService>();
+            services.AddScoped<IMaterialService, MaterialService>();
+            services.AddScoped<IMaterialsPriceService, MaterialsPriceService>();
+            services.AddScoped<IWorkerService, WorkerService>();
+            services.AddScoped<IBrigadeService, BrigadeService>();
+            services.AddScoped<IWorkerSalaryService, WorkerSalaryService>();
         }
 
         public static void AddRepositories(this IServiceCollection services)
         {
             services.AddScoped<IPlanRepository, PlanRepository>();
             services.AddScoped<IPlanDetailRepository, PlanDetailRepository>();
-            /*services.AddScoped<IPlanRepository, PlanRepository>();
-            services.AddScoped<IProviderRepository, ProviderRepository>();*/
+            services.AddScoped<IProviderRepository, ProviderRepository>();
+            services.AddScoped<IMaterialRepository, MaterialsRepository>();
+            services.AddScoped<IMaterialPriceRepository, MaterialsPriceRepository>();
+            services.AddScoped<IWorkerRepository, WorkerRepository>();
+            services.AddScoped<IBrigadeRepository, BrigadeRepository>();
+            services.AddScoped<IWorkerSalaryRepository, WorkerSalaryRepository>();
         }
 
         public static void AddAutoMapper(this IServiceCollection services)
