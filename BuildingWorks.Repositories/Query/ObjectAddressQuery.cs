5d8e6b6503681a7997e453606695222d6a5fb6f6
Ivan Stulskij <ivanstulskij@gmail.com>
2023-02-06 23:40:40 +0300

    created objectAddressQuery

---
+++ BuildingWorks.Repositories/Query/ObjectAddressQuery.cs
@@ -0,0 +1,16 @@
+﻿using BuildingWorks.Databasable.Entities.BuildingObjects.Address;
+using Microsoft.EntityFrameworkCore;
+
+namespace BuildingWorks.Repositories.Query
+{
+    public static class ObjectAddressQuery
+    {
+        public static IQueryable<ObjectAddress> IncludeHierarchy(this IQueryable<ObjectAddress> query)
+        {
+            return query
+                .Include(x => x.Town)
+                .Include(x => x.Region)
+                .Include(x => x.Street);
+        }
+    }
+}
