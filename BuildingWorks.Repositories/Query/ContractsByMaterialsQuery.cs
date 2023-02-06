1be75b7c46554d39d791ad538dbdf600887435bc
Ivan Stulskij <ivanstulskij@gmail.com>
2023-02-06 23:59:38 +0300

    added some overviews and contractsByMaterialsQuery

---
+++ BuildingWorks.Repositories/Query/ContractsByMaterialsQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFramework;
+
+namespace BuildingWorks.Repositories.Query
+{
+    public static class ContractsByMaterials
+    {
+        public static IQueryable<ContractsByMaterials> IncludeHierarchy(this IQueryable<ContractsByMaterials> query)
+        {
+            return query
+                .Include(x => x.Material);
+        }
+    }
+}
