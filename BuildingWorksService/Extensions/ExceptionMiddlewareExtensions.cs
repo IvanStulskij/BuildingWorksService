c284f173438c65d717970636837a03f0c1de9226
Ivan Stulskij <ivanstulskij@gmail.com>
2023-02-10 23:17:27 +0300

    created exceptionMiddlerwareExtensions class

---
+++ BuildingWorksService/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace BuildingWorksService.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureExceptionHandler(this WebApplication application)
+        {
+            application.UseExceptionHandler(error =>
+            {
+                error.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+
+                    if (contextFeature != null)
+                    {
+                        await context.Response.WriteAsync("Internal server error");
+                    }
+                });
+            });
+        }
+    }
+}
