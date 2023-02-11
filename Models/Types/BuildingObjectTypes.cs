91f605e137e09a9832b2f15553d877a1a38c3886
Ivan Stulskij <ivanstulskij@gmail.com>
2023-02-11 15:15:49 +0300

    created building object types

---
+++ Models/Types/BuildingObjectTypes.cs
@@ -0,0 +1,11 @@
+﻿namespace BuildingWorks.Models.Types
+{
+    public static class BuildingObjectTypes
+    {
+        public static List<string> Types = new List<string>
+        {
+            "Образовательное учреждение",
+            "ffffff",
+        };
+    }
+}
