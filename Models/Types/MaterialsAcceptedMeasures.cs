40cdf5b7db27f22346f205212fab062401dd3a61
Ivan Stulskij <ivanstulskij@gmail.com>
2023-02-12 16:39:38 +0300

    created worker salariesErrorMessages

---
+++ Models/Types/MaterialsAcceptedMeasures.cs
@@ -0,0 +1,11 @@
+﻿namespace BuildingWorks.Models.Types
+{
+    public static class MaterialsAcceptedMeasures
+    {
+        public static List<string> Measures = new List<string>
+        {
+            "кг",
+            "м"
+        };
+    }
+}
