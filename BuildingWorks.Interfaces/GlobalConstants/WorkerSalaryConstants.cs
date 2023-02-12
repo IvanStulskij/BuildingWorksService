1767370402563f684cf9acc1c81137ac0f9d48a9
Ivan Stulskij <ivanstulskij@gmail.com>
2023-02-12 16:39:12 +0300

    created worker salary constants

---
+++ BuildingWorks.Interfaces/GlobalConstants/WorkerSalaryConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace BuildingWorks.Common.GlobalConstants
+{
+    public static class WorkerSalaryConstants
+    {
+        public const int ChildenCountMax = 10;
+
+        public const int WorkedDaysMin = 1;
+
+        public const int WorkedDaysMax = 31;
+    }
+}
