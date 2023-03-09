7180b6c26f97bae1bc26d93c4f11029a323f3002
Ivan Stulskij <ivanstulskij@gmail.com>
2023-03-09 22:12:29 +0300

    added exceptionMessages

---
+++ BuildingWorksService/ExceptionMessages.cs
@@ -0,0 +1,9 @@
+﻿namespace BuildingWorksService
+{
+    public static class ExceptionMessages
+    {
+        public const string EntityByIdNotExists = "Entity with such id doesn't exist";
+
+        public const string EntityByConditionNotExists = "Entity by conditions doesn't exist";
+    }
+}
