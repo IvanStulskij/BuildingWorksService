6d68c19a79942bec5020a09adc3c908034190bf6
Ivan Stulskij <ivanstulskij@gmail.com>
2023-03-13 23:58:16 +0300

    added overviewService realization to buildingObjects

--- BuildingWorks.Services/Implementations/BuildingObjects/BuildingObjectService.cs
+++ BuildingWorks.Services/Implementations/BuildingObjects/BuildingObjectService.cs
@@ -1,13 +1,16 @@
 ﻿using AutoMapper;
 using BuildingWorks.Databasable;
+using BuildingWorks.Models;
 using BuildingWorks.Models.Databasable.Tables.BuildingObjects;
+using BuildingWorks.Models.Overview;
 using BuildingWorks.Models.Resources.BuildingObject;
 using BuildingWorks.Repositories.Abstractions.BuildingObjects;
+using BuildingWorks.Services.Interfaces;
 using BuildingWorks.Services.Interfaces.BuildingObjects;
 
 namespace BuildingWorks.Services.Implementations.BuildingObjects
 {
-    public class BuildingObjectService : Service<BuildingObject, BuildingObjectResource, BuildingObjectForm>, IBuildingObjectService
+    public class BuildingObjectService : OverviewService<BuildingObject, BuildingObjectResource, BuildingObjectForm, BuildingObjectOverview>, IBuildingObjectService
     {
         public BuildingObjectService(
             BuildingWorksDbContext context,
