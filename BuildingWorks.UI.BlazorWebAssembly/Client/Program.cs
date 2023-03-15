69e17b7d2490267530ddf58f8620221def99a9c9
Ivan Stulskij <ivanstulskij@gmail.com>
2023-03-15 22:58:46 +0300

    created webassembly

---
+++ BuildingWorks.UI.BlazorWebAssembly/Client/Program.cs
@@ -0,0 +1,11 @@
+using BuildingWorks.UI.BlazorWebAssembly.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7219/api/v1/"/*builder.HostEnvironment.BaseAddress*/) });
+
+await builder.Build().RunAsync();
