d577147e23b00fcdcde01d9e7eda5088aff627a3
Ivan Stulskij <ivanstulskij@gmail.com>
2023-03-16 15:53:13 +0300

    fixed base uri address and added buildingWorksService frontend display

--- BuildingWorks.UI.BlazorWebAssembly/Client/Program.cs
+++ BuildingWorks.UI.BlazorWebAssembly/Client/Program.cs
@@ -6,6 +6,6 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7219/api/v1/"/*builder.HostEnvironment.BaseAddress*/) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5000/api/v1/"/*builder.HostEnvironment.BaseAddress*/) });
 
 await builder.Build().RunAsync();
