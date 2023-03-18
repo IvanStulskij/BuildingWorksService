316cdaaf7995ecfdd8f129e0144f50f872d3220f
Ivan Stulskij <ivanstulskij@gmail.com>
2023-03-18 13:19:12 +0300

    added base path variable

--- BuildingWorks.UI.BlazorWebAssembly/Client/Services/Service.cs
+++ BuildingWorks.UI.BlazorWebAssembly/Client/Services/Service.cs
@@ -6,17 +6,19 @@ namespace BuildingWorks.UI.BlazorWebAssembly.Client.Services
     public abstract class Service<TForm, TResource> : IService<TForm, TResource>
     {
         private readonly HttpClient _http;
-        private readonly string _entityPath;
-
-        public Service(HttpClient http, string entityPath)
+        private readonly string _entity;
+        
+        public Service(HttpClient http, string entity)
         {
             _http = http;
-            _entityPath = entityPath;
+            _entity = entity;
         }
+
+        protected string Path => $"{_http.BaseAddress.AbsolutePath}{_entity}";
 
         public async Task<IEnumerable<TForm>> GetAll(PaginationParameters pagination)
         {
-            var entities = await _http.GetFromJsonAsync<IEnumerable<TForm>>($"{_entityPath}?EntitesPerPage={pagination.EntitiesPerPage}&CurrentPage={pagination.CurrentPage}");
+            var entities = await _http.GetFromJsonAsync<IEnumerable<TForm>>($"{Path}?EntitesPerPage={pagination.EntitiesPerPage}&CurrentPage={pagination.CurrentPage}");
 
             if (entities == null)
             {
@@ -28,34 +30,32 @@ namespace BuildingWorks.UI.BlazorWebAssembly.Client.Services
 
         public async Task Create(TForm entity)
         {
-            await _http.PostAsJsonAsync(_entityPath, entity);
+            await _http.PostAsJsonAsync(_entity, entity);
         }
 
         public async Task Delete(int id)
         {
-            await _http.DeleteAsync($"{_entityPath}/{id}");
+            await _http.DeleteAsync($"{_entity}/{id}");
         }
 
         public async Task Update(TResource entity)
         {
-            await _http.PutAsJsonAsync($"{_entityPath}", entity);
+            await _http.PutAsJsonAsync($"{_entity}", entity);
         }
     }
 
     public abstract class OverviewService<TForm, TResource, TOverivew> : Service<TForm, TResource>, IOverviewService<TForm, TResource, TOverivew>
     {
-        private readonly string _entityPath;
         private readonly HttpClient _http;
 
         public OverviewService(HttpClient http, string entityPath) : base(http, entityPath)
         {
-            _entityPath = entityPath;
             _http = http;
         }
 
         public async Task<IEnumerable<TOverivew>> GetAllOverview(PaginationParameters pagination)
         {
-            var entities = await _http.GetFromJsonAsync<IEnumerable<TOverivew>>($"{_entityPath}/overview?EntitiesPerPage={pagination.EntitiesPerPage}&CurrentPage={pagination.CurrentPage}");
+            var entities = await _http.GetFromJsonAsync<IEnumerable<TOverivew>>($"{Path}/overview?EntitiesPerPage={pagination.EntitiesPerPage}&CurrentPage={pagination.CurrentPage}");
 
             if (entities == null)
             {
