5a2c25c3d6e909425343aeef6bb83654f066c5ca
Ivan Stulskij <ivanstulskij@gmail.com>
2023-03-26 16:48:56 +0300

    created materialPrice and planDetail validators

---
+++ EntityValidator/Implementations/PlanDetailValidator.cs
@@ -0,0 +1,15 @@
+﻿using BuildingWorks.EntityValidator.Abstractions;
+using BuildingWorks.Models.Resources.Plans;
+using FluentValidation;
+
+namespace BuildingWorks.EntityValidator.Implementations
+{
+    public class PlanDetailValidator : EntityValidator<PlanDetailResource>
+    {
+        public PlanDetailValidator() : base()
+        {
+            RuleFor(x => x.Price)
+                .GreaterThan(0);
+        }
+    }
+}
